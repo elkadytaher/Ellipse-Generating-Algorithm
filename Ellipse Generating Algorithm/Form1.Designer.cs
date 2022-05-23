bb1dda913ed92163d45f2e6dfa1e080abcf3ad37
Taher Elkady <70442105+elkadytaher@users.noreply.github.com>
2022-05-23 23:58:11 +0200

    Algorithm Upload

---
+++ Ellipse Generating Algorithm/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace Ellipse_Generating_Algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.rxTextBox = new System.Windows.Forms.TextBox();
+            this.ryTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Center Point";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Radius";
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(145, 86);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(100, 23);
+            this.xTextBox.TabIndex = 2;
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(304, 86);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(100, 23);
+            this.yTextBox.TabIndex = 3;
+            // 
+            // rxTextBox
+            // 
+            this.rxTextBox.Location = new System.Drawing.Point(145, 144);
+            this.rxTextBox.Name = "rxTextBox";
+            this.rxTextBox.Size = new System.Drawing.Size(100, 23);
+            this.rxTextBox.TabIndex = 4;
+            // 
+            // ryTextBox
+            // 
+            this.ryTextBox.Location = new System.Drawing.Point(304, 144);
+            this.ryTextBox.Name = "ryTextBox";
+            this.ryTextBox.Size = new System.Drawing.Size(100, 23);
+            this.ryTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(350, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(185, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "rX";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "rY";
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Location = new System.Drawing.Point(145, 215);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(100, 23);
+            this.DrawButton.TabIndex = 10;
+            this.DrawButton.Text = "Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // Picture
+            // 
+            this.Picture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Picture.Location = new System.Drawing.Point(468, 68);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(320, 257);
+            this.Picture.TabIndex = 11;
+            this.Picture.TabStop = false;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(304, 215);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(100, 23);
+            this.ClearButton.TabIndex = 12;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.Picture);
+            this.Controls.Add(this.DrawButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ryTextBox);
+            this.Controls.Add(this.rxTextBox);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Y";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.TextBox rxTextBox;
+        private System.Windows.Forms.TextBox ryTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ Ellipse Generating Algorithm/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ellipse_Generating_Algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(xTextBox.Text);
+            int y = Convert.ToInt32(yTextBox.Text);
+
+            int rx = Convert.ToInt32(rxTextBox.Text);
+            int ry = Convert.ToInt32(ryTextBox.Text);
+
+            DrawEllipse(x, y, rx, ry);
+
+        }
+
+        private void DrawEllipse(int xCen, int yCen, int Rx, int Ry)
+        {
+            Bitmap bitmap = new Bitmap(this.Width, this.Height);
+            int Rx2 = Rx * Rx;
+            int Ry2 = Ry * Ry;
+            int twoRx2 = 2 * Rx2;
+            int twoRy2 = 2 * Ry2;
+            int px = 0;
+            int p;
+            int y = Ry;
+            int py = twoRx2 * y;
+            int x = 0;
+            p = (int)Math.Round(Ry2 - (Rx2 * Ry)+(0.25*Rx2));
+            while (px < py)
+            {
+                x++;
+                px += twoRy2;
+                if (p < 0)
+                {
+                    p += twoRy2 + px;
+                }else
+                {
+                    y--;
+                    py -= twoRx2;
+                    p += Ry2 + px - py;
+
+                }
+                
+                bitmap.SetPixel(xCen + x, yCen + y, Color.Red);
+                bitmap.SetPixel(xCen - x, yCen + y, Color.Red);
+                bitmap.SetPixel(xCen + x, yCen - y, Color.Red);
+                bitmap.SetPixel(xCen - x, yCen - y, Color.Red);
+
+            }
+            while (y > 0)
+            {
+                y--;
+                py -= twoRx2;
+                if (p > 0)
+                {
+                    p += Rx2 - py;
+                }
+                else
+                {
+                    x++;
+                    px += twoRy2;
+                    p += Rx2 - py + px;
+                }
+                bitmap.SetPixel(xCen + x, yCen + y, Color.Red);
+                bitmap.SetPixel(xCen - x, yCen + y, Color.Red);
+                bitmap.SetPixel(xCen + x, yCen - y, Color.Red);
+                bitmap.SetPixel(xCen - x, yCen - y, Color.Red);
+
+            }
+            Picture.Image = bitmap;
+        }
+
+        private void plotPoint(int xCen, int yCen, int x, int y)
+        {
+            Bitmap bitmap = new Bitmap(this.Width, this.Height);
+            bitmap.SetPixel(xCen + x, yCen + y,Color.Red);
+            bitmap.SetPixel(xCen - x, yCen + y, Color.Red);
+            bitmap.SetPixel(xCen + x, yCen - y, Color.Red);
+            bitmap.SetPixel(xCen - x, yCen - y, Color.Red);
+
+            Picture.Image = bitmap;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Picture.Image = null;
+            xTextBox.Clear();
+            yTextBox.Clear();
+            rxTextBox.Clear();
+            ryTextBox.Clear();
+        }
+    }
+}
